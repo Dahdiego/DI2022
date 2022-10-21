f13e2c0fced2b5f0c7993785acf930c4bfbe2671
DiegoAlonsoHernando <DiegoAlonsoHernando@INF04-01>
2022-10-21 12:15:18 +0200

    Creación de proyecto

---
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,19 @@
+
+package pruebagithub;
+
+/**
+ *
+ * @author DiegoAlonsoHernando
+ */
+public class PruebaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
